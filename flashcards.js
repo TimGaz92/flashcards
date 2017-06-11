cffb7d57d98014032ffdd6757df933f3d5047b28
TimGaz92 <tgaziano@optonline.net>
2017-06-11 15:35:04 -0400

    minor documentation changes

--- flashcards.js
+++ flashcards.js
@@ -33,31 +33,27 @@ function TriviaCard(question, responce, cardNum){
 								user.score--;
 								console.log("your score is: " + user.score);
 								k = 0;
+								askQuestions();
 							}
-//SOMETHING WENT WRONG WITH THE ANSWERS, IT NO LONGER RECOGNIZES INCORRECT RESPONCES
 		});
-		// askQuestions();
 	}
 }	
 var user = {
 	score:0,
 }
-
+//these are the pre-set standard questions
 var card1 = new TriviaCard("Who's the best developer?", "tim", 1);
 var card2 = new TriviaCard("What does S.T.I.G stand for?", "secure technical information guidelines", 2);
 var card3 = new TriviaCard("which is better a CASP or a Security+?", "CASP", 3);
 var card4 = new TriviaCard("what state is Ft. Huachuca in?", "Arizona", 4);
 var card5 = new TriviaCard("8 * 2 = ", "16", 5);
-
-
+var card6 = new TriviaCard("Brian Griffen is a...?", "dog", 6);
 
 cardArray.push(card1, card2, card3, card4, card5);
 for (var i = 0; i < cardArray.length; i++) {
 	file.appendFile("questionLogFile.txt", cardArray[i].question + "_" + cardArray[i].responce + " | ", function(err){
 		}); 
 }
-
-// console.log(cardArray);
 
 if (process.argv[2] === 'new') {
 	console.log("use the following format: Question, Answer, questionNumber");
@@ -68,18 +64,15 @@ if (process.argv[2] === 'new') {
 	cardArray.push(userCard);
 }
 
-//ask questions function
 
 if (process.argv[2] === "?") {
 	askQuestions();
 }
-//all the questions are runnign at once, check the recursive examples from class
 
+//ask questions function
 function askQuestions(){
 	console.log("starting questions, get them correct to move on, else start over");
-		//k = 0;
 		cardArray[k].ask();
-//this is only asking the first question
 }
 
 
